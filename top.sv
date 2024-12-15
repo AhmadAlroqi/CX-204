69559c073c312c0c4e384ea4c2eee5092710c1f8
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-12-15 13:45:23 +0300

    Add files via upload

---
+++ top.sv
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module top #(
+    parameter  IMEM_DEPTH = 4,       
+    parameter  REGF_WIDTH = 16,     
+    parameter  ALU_WIDTH = 16,       
+    parameter  PROG_VALUE = 3
+
+    );
+    
+      
+  
+  
+  
+endmodule
+
