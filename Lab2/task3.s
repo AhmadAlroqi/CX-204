bda06c094cd0e2a860030e4e2fb9431fcb67de5b
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-12-17 15:00:07 +0300

    Update task3.s

--- Lab2/task3.s
+++ Lab2/task3.s
@@ -10,3 +10,22 @@ xor x10,x9,x8
 addi a1, x10,0
 li a0, 1
 ecall
+
+#Task3.3
+li x5,13#1101
+srli x6,x5,16
+xor x5,x5,x6
+srli x6,x5,8
+xor x5,x5,x6
+srli x6,x5,4
+xor x5,x5,x6
+srli x6,x5,2
+xor x5,x5,x6
+srli x6,x5,1
+xor x5,x5,x6
+andi x10,x5,1
+
+#print 
+addi a1, x10,0
+li a0, 1
+ecall
